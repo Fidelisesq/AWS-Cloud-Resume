b7a83f8ee94efda16adbbedd835bdc3e97d63f8f
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-02-17 16:51:52 +0000

    Library_import

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -42,5 +42,21 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
 
 
