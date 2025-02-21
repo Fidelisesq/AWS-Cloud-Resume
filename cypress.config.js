76e795d3842d84fbe01091ee75ee3f1c00aa4930
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-02-21 21:39:01 +0000

    Auto-Testing

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: 'qsvn1w',  // Add your project ID here
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
