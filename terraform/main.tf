51d57a55b93335cf6c279da7776e91045b3db08e
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-02-27 13:10:59 +0000

    WAF Deployment + Lambda_Test

--- terraform/main.tf
+++ terraform/main.tf
@@ -157,6 +157,9 @@ resource "aws_cloudfront_distribution" "cloud_resume_distribution" {
 resource "aws_wafv2_web_acl_association" "cloudfront_waf_association" {
   resource_arn = aws_cloudfront_distribution.cloud_resume_distribution.arn
   web_acl_arn   = aws_wafv2_web_acl.cloudfront_waf.arn
+  
+  depends_on = [aws_cloudfront_distribution.cloud_resume_distribution,
+  aws_wafv2_web_acl.cloudfront_waf]
 }
 
 
