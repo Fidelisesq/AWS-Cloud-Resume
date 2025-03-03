0b501c6722a34591abe92f6ec66cd9e5a8cdf904
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-03-03 18:03:15 +0000

    WAF RateLimit

--- terraform/main.tf
+++ terraform/main.tf
@@ -850,7 +850,7 @@ resource "aws_wafv2_web_acl" "cloudfront_waf" {
 
     statement {
       rate_based_statement {
-        limit              = 350
+        limit              = 100
         aggregate_key_type = "IP"
       }
     }
